2025ece05b5fb8b3db6a10909202c5fed75d1c90
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-09-04 20:32:30 +0300

    order page and checkout process

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
-pk_test_51Le1U8C3GThXvK4R1bMdsj4PQHBtRUa0ECcAwaCaQeGvgIXJMMjoUqTwNrdsUlod6aFBfkV3LFC0gOddSiR3H4Xh00bFDs5tol
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -39,12 +46,22 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
